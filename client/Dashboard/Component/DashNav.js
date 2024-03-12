a7cf8007d341a1f8a77d33d9a93e7634c5eefb9d
Ferrari joris <dedjoris09@gmail.com>
2024-03-12 12:17:07 +0100

    main

--- client/Dashboard/Component/DashNav.js
+++ client/Dashboard/Component/DashNav.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Modal } from 'react-native';
 import MenuCreate from './MenuCreate';
 import Menu from './Menu';
 import { FontAwesome } from '@expo/vector-icons';
 
-const DashNav = ({createProject,listProject, deleteProject, isdeleteProject, deleteOpen, isDeleteOpen}) => {
+const DashNav = ({ createProject, listProject, deleteProject, isdeleteProject, deleteOpen, isDeleteOpen, openNav }) => {
   const numList = () => {
     return (
-      <View style={style.projectContainer}>
+      <View style={styles.projectContainer}>
         {listProject.map((project, index) => (
-          <View key={index} style={style.projectStyle}>
+          <View key={index} style={styles.projectStyle}>
             <Text>project {index}</Text>
-            {isDeleteOpen && <TouchableOpacity style={style.boutonTrashStyle} onPress={() => deleteProject(index)}>
+            {isDeleteOpen && <TouchableOpacity style={styles.boutonTrashStyle} onPress={() => deleteProject(index)}>
               <FontAwesome name={"trash"} size={18} color={"white"} />
             </TouchableOpacity>}
           </View>
@@ -19,40 +19,84 @@ const DashNav = ({createProject,listProject, deleteProject, isdeleteProject, del
       </View>
     );
   };
+
   return (
-    <View style={style.container}>
-      <Text style={style.textStyle}>Your Board</Text>
-      <View style={style.menuContenair}>
-        <Menu createProject={createProject} deleteProject={deleteProject} deleteOpen={deleteOpen}/>
+    <Modal
+      animationType="slide"
+      transparent={true}
+      visible={isdeleteProject}
+      onRequestClose={() => deleteOpen(false)} // Ferme la modal si l'utilisateur appuie sur le bouton de retour Android
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.modalContent}>
+          <Text style={styles.title}>Your Board</Text>
+          <TouchableOpacity onPress={openNav} style={styles.closeButton}>
+            <FontAwesome name="times" style={styles.closeModal} />
+          </TouchableOpacity>
+          <Menu
+            createProject={createProject}
+            deleteProject={deleteProject}
+            deleteOpen={deleteOpen}
+          />
+          {listProject ? numList() : null}
+        </View>
       </View>
-      {listProject? numList(listProject): null}
-    </View>
+    </Modal>
   );
 };
 
-const style = StyleSheet.create({
-    container: {
-        backgroundColor: '#A1B5FE',
-        width: '30%',
-        height: '100%',
-      },
-    menuContenair:{
-        flexDirection: "row",
-    },
-    textStyle: {
-      backgroundColor: "#7B8BC7",
-      height: 35,
-      paddingTop: 8,
-      paddingLeft: 20,
-      color:"#FFFFFF",
-    },
-    projectContainer:{
-      height: "100%"
-    },
-    projectStyle:{
-      marginTop: 20,
-      paddingLeft: 10,
-    },
-})
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    height: '30%',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    paddingHorizontal: 10,
+    paddingTop: 10,
+  },
+  title: {
+    backgroundColor: "#F5F7FB",
+    height: 35,
+    paddingTop: 8,
+    paddingLeft: 20,
+    color: "black",
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    marginBottom: 10,
+    fontWeight: "bold"
+  },
+  menuContainer: {
+    flexDirection: "row",
+  },
+  projectContainer: {
+    marginTop: 10,
+  },
+  projectStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  boutonTrashStyle: {
+    backgroundColor: 'red',
+    borderRadius: 20,
+    padding: 5,
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  closeModal: {
+    fontSize: 25,
+    color: "#AD002C",
+    padding: 3
+  }
+});
 
 export default DashNav;
--- client/Dashboard/Component/navBar.js
+++ client/Dashboard/Component/navBar.js
@@ -1,21 +1,106 @@
-import React from 'react';
-import { View, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TouchableOpacity, StyleSheet, Image, Modal, Text, Button } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
 
-const NavBar = ({OpenNav}) => {
+const NavBar = ({ openNav, isNavOpen }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get('url_de_votre_api')
+  //     .then(response => {
+  //       console.log('Users:', response.data)
+  //       setUsers(response.data);
+  //     })
+  //     .catch(error => {
+  //       setUsers([]);
+  //       console.error('Error fetching users:', error);
+  //     });
+  // }, []);
+
+  const openProfile = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+  //console.log(users)
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={OpenNav}>
-        <FontAwesome name="list" size={24} color="white" />
-      </TouchableOpacity>
+      <View style={styles.leftContainer}>
+        <TouchableOpacity onPress={openNav}>
+          <FontAwesome name="list" size={24} color="#BABBBF" />
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.rightContainer}>
+        {users && users.length > 0 ? (
+          users.map(user => (
+            <TouchableOpacity key={user.id} onPress={openProfile}>
+              <Image
+                source={{ uri: user.avatar }}
+                style={styles.profileImage}
+              />
+            </TouchableOpacity>
+          ))
+        ) : (
+          <TouchableOpacity onPress={openProfile}>
+            <Image
+              source={require('../../assets/user.png')}
+              style={styles.profileImage}
+            />
+          </TouchableOpacity>
+        )}
+      </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={[styles.modalContainer, styles.modalContent]}>
+          <Text>This is a modal.</Text>
+          <Button title="Close" onPress={closeModal} />
+        </View>
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:40,
-    paddingLeft:10,
+    flexDirection: 'row',
+    paddingTop: 40,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  leftContainer: {
+    flex: 1,
+    alignItems: 'flex-start',
+  },
+  rightContainer: {
+    flex: 1,
+    alignItems: 'flex-end',
+  },
+  profileImage: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    paddingHorizontal: 20,
+  },
+  modalContent: {
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
   },
 });
 
